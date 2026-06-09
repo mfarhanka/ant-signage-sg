b5acbca0ca8ab0058a05e7617e5e0b0627985dd6
Farhan Kamarzaman <farhan@dzvisual.my>
2026-06-09 11:53:09 +0800

    feat: Add favicon link for improved branding and user experience

--- 1/1/index.php
+++ 1/1/index.php
@@ -16,6 +16,8 @@
     <!-- FontAwesome Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
+    <link rel="icon" type="image/png" href="assets/images/logo.png">
+
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body class="d-flex flex-column h-full">
